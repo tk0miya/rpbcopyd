75a9a0ac0e421c6be8d83f9ced681c592e04c2e8
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-08-03 16:35:53 +0900

    Parse command line options by clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    #[arg(short, long, default_value_t = false)]
+    daemon: bool,
+
+    #[arg(short = 'H', long, default_value = "localhost")]
+    host: String,
+
+    #[arg(short, long, default_value_t = 12345)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!(
+        "daemon: {:?}, host: {:?}, port: {:?}",
+        args.daemon, args.host, args.port
+    );
 }
