d23514a2139d3aa7749f345b9f591de46bfafbc8
Takeshi KOMIYA <i.tkomiya@gmail.com>
2024-08-04 11:31:22 +0900

    rpbpaste - pbpaste command for remote

---
+++ src/rpbpaste.rs
@@ -0,0 +1,26 @@
+use clap::Parser;
+use reqwest;
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    #[arg(short = 'H', long, default_value = "host.docker.internal")]
+    host: String,
+
+    #[arg(short, long, default_value_t = 12345)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let url = format!("http://{}:{}", args.host, args.port);
+
+    match reqwest::get(url).await {
+        Ok(response) => {
+            let body = response.text().await.unwrap();
+            print!("{}", body);
+        }
+        Err(e) => eprintln!("Error: {}", e),
+    }
+}
